984eaea2b74deb09360553f6882b26cdb6e4f66d
Sina Golbarg <46792968+sinagolbarg@users.noreply.github.com>
2026-04-12 11:43:14 -0400

    Update train.py

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ os.makedirs("artifacts", exist_ok=True)
 def main():
     iris = load_iris()
     X, y = iris.data, iris.target
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 
     model = LogisticRegression(max_iter=200)
     model.fit(X_train, y_train)
